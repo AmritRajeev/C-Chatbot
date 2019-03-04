fb8993452987fc97d63f2786e659cdf4bee3d3ab
AmritRajeev <44172582+AmritRajeev@users.noreply.github.com>
2019-03-04 16:26:37 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <windos.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main()
+{
+char name[50];
+int in,t=1;
+while(t!=2){
+printf("Hello!! Welcome My name is JARVIS and what's your name?");
+scanf("%s",&name);
+printf(Hello %s , What whould you like to do today? '/n'" );
+pritnf("Oh and don't forget to be connected to the internet")
+printf("Need help !! Here is what you can do ");
+printf("1) A quick fact check '/n' 2)Play a game3  '\n' 3)Google Some thing 4)Play some song 5)Simply chat 6)Exit ");
+scanf("%d".&in);
+switch (in){
+    case '1'{}
+    case '2'{}
+    case '3'{}
+    case '4'{}
+    case '5'{}
+    case '6'
+            printf("That's Sad Let's Talk again later ");
+            i++;
+            break ;
+            }
+    break;
+           }
+}
